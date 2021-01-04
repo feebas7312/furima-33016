d3aed681236d3c9cd3298c3c1909d17b5c8b038d
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-04 19:38:21 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
+
+  VALID_FULL_WIDTH_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  with_options presence: true, format: { with: VALID_FULL_WIDTH_REGEX, message: "Full-width characters" } do
+    validates :first_name
+    validates :last_name
+  end
+
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/
+  with_options presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
 end
