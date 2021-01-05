0ba58fa3031fb50ed626176439848621343d39b9
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-05 23:16:24 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options numericality: true, do
+    validates :category
+    validates :sales_status
+    validates :shipping_fee_status
+    validates :prefecture
+    validates :scheduled_delivery
+  end
 end
