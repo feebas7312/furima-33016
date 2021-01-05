bb24427fdfcdddf2daf779e5e2f2707a73840d4c
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-06 01:56:18 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,22 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  with_options numericality: true do
-    validates :category
-    validates :sales_status
-    validates :shipping_fee_status
-    validates :prefecture
-    validates :scheduled_delivery
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price
   end
+
+  validates :price, numericality: { only_integer: true, message: "Half-width number" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Out of setting range" }
+
+  with_options numericality: { message: "Select" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
 end
