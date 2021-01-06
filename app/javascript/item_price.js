4d6cf448f9fa72c2229d50c0b7b2d4572784f3de
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-06 16:53:02 +0900

    出品金額に計算機能追加

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+function itemPrice(){
+
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+    addTaxDom.innerHTML = Math.floor(inputValue) / (10);
+    profit.innerHTML = Math.floor(inputValue) - (addTaxDom.innerHTML);
+  });
+
+}
+window.addEventListener('load', itemPrice);
